fee6f5436077bc27917d8098d869969375c67b2f
Daniel Heinrich Schmidt <schmidt.daniel.h@gmail.com>
2026-03-04 12:50:25 +0200

    fixed logic of ltv from yearly ot all time ltv. renamed files accordingly

--- Olist_Project_SQL_Files/QUERY_avg_ltv.sql
+++ Olist_Project_SQL_Files/QUERY_avg_ltv.sql
@@ -1,14 +1,12 @@
 -- Average customer lifetime value (LTV) by year
 WITH ltv AS (
-    SELECT DATE_TRUNC('year', ord.order_purchase_timestamp) AS year,
-        cus.customer_unique_id,
+    SELECT cus.customer_unique_id,
         SUM(pay.payment_value) AS revenue_per_customer
     FROM olist_customers_dataset AS cus
         INNER JOIN olist_orders_dataset AS ord ON ord.customer_id = cus.customer_id
         INNER JOIN olist_order_payments_dataset AS pay ON pay.order_id = ord.order_id
     WHERE ord.order_status = 'delivered'
-    GROUP BY cus.customer_unique_id,
-        year
+    GROUP BY cus.customer_unique_id
     ORDER BY revenue_per_customer DESC
 )
 SELECT ROUND(AVG(revenue_per_customer), 2) AS avg_ltv
--- Olist_Project_SQL_Files/QUERY_median_ltv.sql
+++ Olist_Project_SQL_Files/QUERY_median_ltv.sql
@@ -1,23 +1,19 @@
 -- median customer lifetime value (LTV) by year
 WITH median AS (
-    SELECT year,
-        unique_customer,
+    SELECT unique_customer,
         revenue_per_customer,
         ROW_NUMBER() OVER (
-            PARTITION BY year
             ORDER BY revenue_per_customer
         ) AS rn,
-        COUNT(*) OVER (PARTITION BY year) AS cnt
+        COUNT(*) OVER () AS cnt
     FROM (
-            SELECT DATE_TRUNC('year', ord.order_purchase_timestamp) AS year,
-                cus.customer_unique_id unique_customer,
+            SELECT cus.customer_unique_id unique_customer,
                 SUM(pay.payment_value) AS revenue_per_customer
             FROM olist_customers_dataset AS cus
                 INNER JOIN olist_orders_dataset AS ord ON ord.customer_id = cus.customer_id
                 INNER JOIN olist_order_payments_dataset AS pay ON pay.order_id = ord.order_id
             WHERE ord.order_status = 'delivered'
-            GROUP BY cus.customer_unique_id,
-                year
+            GROUP BY cus.customer_unique_id
             ORDER BY revenue_per_customer DESC
         ) AS ltv
 )
