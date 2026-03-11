6a5d99e1a284489453731f765575f406f99b8494
Daniel Heinrich Schmidt <schmidt.daniel.h@gmail.com>
2026-03-11 15:51:07 +0200

    removed redundant queries

--- Olist_Project_SQL_Files/QUERY_new_customers_monthly.sql
+++
@@ -1,8 +0,0 @@
--- How many new customers are aquired each month
-SELECT COUNT(DISTINCT customer_unique_id) AS new_customers,
-    DATE_TRUNC('month', ord.order_purchase_timestamp) AS year_month
-FROM olist_customers_dataset AS cus
-    INNER JOIN olist_orders_dataset AS ord ON ord.customer_id = cus.customer_id --WHERE ord.order_status = 'delivered'
-GROUP BY year_month
-HAVING COUNT(DISTINCT customer_unique_id) > 500
-ORDER BY year_month;
--- Olist_Project_SQL_Files/QUERY_repeat_customers_monthly.sql
+++
@@ -1,7 +0,0 @@
--- how many return customers per month
-SELECT DATE_TRUNC('month', ord.order_purchase_timestamp) AS month,
-    COUNT(cus.customer_unique_id) - COUNT(DISTINCT cus.customer_unique_id) AS return_customers
-FROM olist_customers_dataset cus
-    INNER JOIN olist_orders_dataset ord ON ord.customer_id = cus.customer_id
-GROUP BY month
-HAVING COUNT(ord.order_purchase_timestamp) > 500;
